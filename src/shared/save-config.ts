e14ff19cd90f71f56ab378aeb22ec961ace1f0b6
Kerren Ortlepp <kerren@entrostat.com>
2022-12-13 17:36:11 +0200

    Updated the save config to ensure it doesn't create a file instead of a folder

--- src/shared/save-config.ts
+++ src/shared/save-config.ts
@@ -2,10 +2,6 @@ import * as fs from 'fs-extra';
 import * as path from 'node:path';
 
 export async function saveConfig(filePath: string, config: any): Promise<any> {
-    try {
-        await fs.mkdirp(path.dirname(filePath));
-        return await fs.writeJson(filePath, config);
-    } catch {
-        return {};
-    }
+    await fs.mkdirp(path.dirname(filePath));
+    return await fs.writeJson(filePath, config);
 }
