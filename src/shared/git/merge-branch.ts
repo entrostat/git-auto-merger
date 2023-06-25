b9f0b4b52b286f2abbf1cc10365d18c6eae187a6
Kerren Ortlepp <kerren@entrostat.com>
2023-06-25 13:10:31 +0200

    fix(logging): Added logging for the commit and push flags

--- src/shared/git/merge-branch.ts
+++ src/shared/git/merge-branch.ts
@@ -13,9 +13,8 @@ export async function mergeBranch(
             console.log,
             console.error,
         );
-        await executeCommand(`git pull`, console.log, console.error);
         const message = await executeCommand(
-            `git merge --no-edit --verbose --allow-unrelated-histories ${
+            `git merge --no-edit --verbose --no-ff --allow-unrelated-histories ${
                 shouldCommit || shouldPush ? '' : '--no-commit'
             } ${baseBranch}`,
             console.log,
@@ -23,9 +22,8 @@ export async function mergeBranch(
         );
         if (shouldPush) {
             await executeCommand('git push', () => {}, console.error);
-        } else if (!shouldCommit) {
-            await safeCommand('git merge --abort', () => {}, console.error);
         }
+        await safeCommand('git merge --abort', () => {}, console.error);
 
         return { error: false, message };
     } catch (e) {
